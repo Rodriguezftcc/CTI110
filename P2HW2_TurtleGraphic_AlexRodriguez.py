1f131e9e213d55aad74357f33bb423719f6d92be
Rodriguezftcc <55244460+Rodriguezftcc@users.noreply.github.com>
2019-09-27 19:54:09 -0400

    Add files via upload

---
+++ P2HW2_TurtleGraphic_AlexRodriguez.py
@@ -0,0 +1,55 @@
+# This will be making the olympic rings with turtle  
+# 09/24/2019
+# CTI-110 P2HW2 - Turtle Graphic
+# Alex Rodriguez
+
+#This will start turtle
+import turtle
+
+#the turtle will go up without drawing
+turtle.penup()
+#the turtle will go to position -110, -25
+turtle.goto(-110, -25)
+#the turtle will go down without drawing
+turtle.pendown()
+#the turtle will make a 45 diameter
+turtle.circle(45)
+
+#the turtle will go up without drawing
+turtle.penup()
+#the turtle will go to position 0,-25
+turtle.goto(0, -25)
+#the turtle will go down without drawing
+turtle.pendown()
+#the turtle will make a 45 diameter
+turtle.circle(45)
+
+#the turtle will go up without drawing
+turtle.penup()
+#the turtle will go to position 110, -25
+turtle.goto(110, -25)
+#the turtle will go down without drawing
+turtle.pendown()
+#the turtle will make a 45 diameter
+turtle.circle(45)
+
+#the turtle will go up without drawing
+turtle.penup()
+#the turtle will go to position -55, -75
+turtle.goto(-55, -75)
+#the turtle will go down without drawing
+turtle.pendown()
+#the turtle will make a 45 diameter
+turtle.circle(45)
+
+#the turtle will go up without drawing
+turtle.penup()
+#the turtle will go to position 55, -75
+turtle.goto(55, -75)
+#the turtle will go down without drawing
+turtle.pendown()
+#the turtle will make a 45 diameter
+turtle.circle(45)
+#Turtle finishes
+turtle.done()
+ 
